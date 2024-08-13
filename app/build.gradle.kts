02410fd4c3fe897d7718a2067670284fb5484597
CristianATZ <cristiantorreszvl@gmail.com>
2024-08-13 00:29:26 -0600

    Modificar minimo SDK de 24 a 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.devtorres.taskalarm"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.6.2"
